d0d14fc145d94ceab285ce5cfebf2e8cf736f456
Muneeb <muneeb0239@gmail.com>
2025-09-03 02:34:06 +0500

    add swagger and also fix api running problems

--- chit-chat-api/Program.cs
+++ chit-chat-api/Program.cs
@@ -1,19 +1,22 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title="Chit Chat Api",Version="v1" }); });
 
 var app = builder.Build();
-// test git
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 
 
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chit Chat Api");
+    });
 }
 
 app.UseHttpsRedirection();
